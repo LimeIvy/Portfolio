dea88cdfc8eba713ac71a5a5d4876960565f30b8
LimeIvy <raimu10311221@yahoo.co.jp>
2025-04-22 22:41:24 +0900

    🐛 productsの更新

--- utils/products.tsx
+++ utils/products.tsx
@@ -1,30 +1,16 @@
 export const products = [
-    {
-      name: "Matchai",
-      image: "/images/matchai.png",
-      content:
-        "SysHack2025にて、同じ研究室の3人チームで作成した、待ち合わせをもっと楽にするアプリケーションです。 現在テスト用サイトを公開しています。",
-      url: "https://github.com/LimeIvy/MATCHAI",
-    },
-    {
-      name: "Product 2",
-      image: "/images/matchai.png",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-      url: "https://matchai.com",
-    },
-    {
-      name: "Product 3",
-      image: "/images/matchai.png",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-      url: "https://matchai.com",
-    },
-    {
-      name: "Product 4",
-      image: "/images/matchai.png",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-      url: "https://matchai.com",
-    },
-  ];
+  {
+    name: "MATCHAI",
+    image: "/images/Matchai.png",
+    content:
+      "SysHack2025にて、同じ研究室の3人チームで作成した、待ち合わせをもっと楽にするアプリケーションです。 現在テスト用サイトを公開しています。",
+    url: "https://github.com/LimeIvy/MATCHAI",
+  },
+  {
+    name: "Wind Quest",
+    image: "/images/WindQuest.png",
+    content:
+      "自分のTailwindCSSスキル向上のために、ゲーム性が大きい勉強サイトが欲しく作成しました。 現在鋭意開発中です。",
+    url: "https://koki-portforio.vercel.app/",
+  },
+];
